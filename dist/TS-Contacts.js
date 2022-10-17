4884cd67ce7b2ed42b0aee293ab87bd9309a2d6c
Rootdiv <rootdiv@mail.ru>
2022-10-17 17:18:04 +0300

    feat: lesson 3 task 4

--- dist/TS-Contacts.js
+++ dist/TS-Contacts.js
@@ -1,45 +1,56 @@
 "use strict";
-const isUser = (val) => 'group' in val;
 const persons = [
     {
+        type: 'user',
         name: 'Иван Петров',
         age: 27,
         group: 'SEO-специалист',
     },
     {
+        type: 'user',
         name: 'Марат Aляуддинов',
         age: 20,
         group: 'Музыкант',
     },
     {
+        type: 'user',
         name: 'Роман Смирнов',
         age: 30,
         group: 'React-разработчик',
     },
     {
+        type: 'user',
         name: 'Ульяна Никитина',
         age: 25,
         group: 'Дизайнер',
     },
     {
+        type: 'user',
         name: 'Владислав Горлов',
         age: 35,
         group: 'JavaScript-разработчик',
     },
     {
+        type: 'admin',
         name: 'Марк Кузнецов',
         age: 40,
         role: 'Администратор',
     },
 ];
+const isAdmin = (person) => person.type === 'admin';
+const isUser = (person) => person.type === 'user';
 const logPerson = (person) => {
-    let information;
-    if (!isUser(person)) {
+    let information = '';
+    if (isAdmin(person)) {
         information = person.role;
     }
-    else {
+    if (isUser(person)) {
         information = person.group;
     }
-    console.log(`${person.name}, ${person.age}, ${information}`);
+    console.log(` - ${person.name}, ${person.age}, ${information}`);
 };
-persons.forEach(logPerson);
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+console.log();
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
--- src/TS-Contacts.ts
+++ src/TS-Contacts.ts
@@ -1,60 +1,75 @@
 interface User {
+  type: 'user';
   name: string;
   age: number;
-  group?: string;
+  group: string;
 }
 
 interface Admin {
+  type: 'admin';
   name: string;
   age: number;
-  role?: string;
+  role: string;
 }
 
 type Person = User | Admin;
 
-const isUser = (val: User | Admin): val is User => 'group' in val;
-
 const persons: Person[] = [
   {
+    type: 'user',
     name: 'Иван Петров',
     age: 27,
     group: 'SEO-специалист',
   },
   {
+    type: 'user',
     name: 'Марат Aляуддинов',
     age: 20,
     group: 'Музыкант',
   },
   {
+    type: 'user',
     name: 'Роман Смирнов',
     age: 30,
     group: 'React-разработчик',
   },
   {
+    type: 'user',
     name: 'Ульяна Никитина',
     age: 25,
     group: 'Дизайнер',
   },
   {
+    type: 'user',
     name: 'Владислав Горлов',
     age: 35,
     group: 'JavaScript-разработчик',
   },
   {
+    type: 'admin',
     name: 'Марк Кузнецов',
     age: 40,
     role: 'Администратор',
   },
 ];
 
+const isAdmin = (person: Person): person is Admin => person.type === 'admin';
+
+const isUser = (person: Person): person is User => person.type === 'user';
+
 const logPerson = (person: Person) => {
-  let information: string | undefined;
-  if (!isUser(person)) {
+  let information: string = '';
+  if (isAdmin(person)) {
     information = person.role;
-  } else {
+  }
+  if (isUser(person)) {
     information = person.group;
   }
-  console.log(`${person.name}, ${person.age}, ${information}`);
+  console.log(` - ${person.name}, ${person.age}, ${information}`);
 };
 
-persons.forEach(logPerson);
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
